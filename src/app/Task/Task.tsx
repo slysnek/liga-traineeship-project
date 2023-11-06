c96747e502611cd3fac237df586092d4393432f0
slysnek <bloodwingest@gmail.com>
2023-11-06 23:25:58 +0300

    refactor: small changes

--- src/app/Task/Task.tsx
+++ src/app/Task/Task.tsx
@@ -3,13 +3,13 @@ import pencilIcon from '../../assets/icons/icon-pencil.svg';
 import crossIcon from '../../assets/icons/icon-cross.svg';
 import checkmarkIcon from '../../assets/icons/icon-checkmark.svg';
 import styles from './Task.module.css';
-import { ITaskType } from './TaskTypes.types';
+import { ITaskType } from './Task.types';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const Task: React.FC<ITaskType> = (taskData: ITaskType) => {
   return (
     <li className={styles.task}>
-      <h2 className={styles.name}>{taskData.name}</h2>
+      <h3 className={styles.name}>{taskData.name}</h3>
       <Link className={styles.edit} to={`add_task/${taskData.id}`}>
         <img className={styles.icon} src={pencilIcon} alt="pencil icon" />
       </Link>
