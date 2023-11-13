c4957955e1af15ec735fa782bdf8162235856613
Kirill <bloodwingest@gmail.com>
2023-11-13 20:59:36 +0300

    refactor: delete console.logs

--- src/api/apiController.ts
+++ src/api/apiController.ts
@@ -20,27 +20,22 @@ export default class Controller {
 
   async getData(filters: GetFilteredTasksQuery): Promise<IGetTasksResponse> {
     const response = await this.dataFetcher.getData(this.url, filters);
-    console.log('Getting filtered data:');
     return response;
   }
   async getDataById(id: number): Promise<IGetTaskResponse> {
     const response = await this.dataFetcher.getDataById(this.url, id);
-    console.log('Getting filtered data:');
     return response;
   }
   async addData(formData: AddTaskQuery): Promise<IPostTaskResponse> {
     const response: IPostTaskResponse = await this.dataFetcher.addData(this.url, formData);
-    console.log('Adding data:');
     return response;
   }
   async changeData(formData: ChangeTaskQuery): Promise<IPatchTaskResponse> {
     const response = await this.dataFetcher.changeData(this.url, formData);
-    console.log('Changing data:');
     return response;
   }
   async deleteData(id: number): Promise<void> {
     const response = await this.dataFetcher.deleteData(this.url, id);
-    console.log('Deleted data.');
     return response;
   }
 }
