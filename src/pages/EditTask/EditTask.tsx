ec0c6097cb02dd4d6c3c728195b81bb5bb9235ac
Kirill <bloodwingest@gmail.com>
2023-11-13 17:34:23 +0300

    feat: add getting tasks to edit page

--- src/pages/EditTask/EditTask.tsx
+++ src/pages/EditTask/EditTask.tsx
@@ -1,18 +1,38 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import taskNotFound from '../../assets/gif/task-not-found.gif';
 import styles from './EditTask.module.css';
 import Form from 'app/Form/Form';
 import { FormButton } from 'app/Form/Form.types';
 import TitleLink from 'components/TitleLink/TitleLink';
-import { useAppSelector } from 'src/hooks/hooks';
+import { useAppDispatch, useAppSelector } from 'src/hooks/hooks';
+import { getTasksQuery } from 'src/store/tasksSlice';
+import { Loader } from 'components/Loader';
 
 const EditTask = () => {
+  const dispatch = useAppDispatch();
   const { id } = useParams();
-  const taskToEdit = useAppSelector((state) => state.tasksInStore.tasks.find((task) => task.id === Number(id)));
+  const tasks = useAppSelector((state) => state.tasksInStore.tasks);
+  const task = tasks.find((task) => task.id === Number(id));
+
+  useEffect(() => {
+    dispatch(getTasksQuery({}));
+  }, []);
+
+  if (!tasks.length) {
+    return (
+      <div className={styles.loader}>
+        <Loader isLoading={true}>
+          <h2>Loading...</h2>
+        </Loader>
+      </div>
+    );
+  }
+
   return (
     <>
       <TitleLink link="/" buttonName="Back to tasks"></TitleLink>
-      {taskToEdit ? (
+      {task ? (
         <Form taskId={Number(id)} type={'Edit task' as FormButton}></Form>
       ) : (
         <div className={styles.taskNotFound}>
