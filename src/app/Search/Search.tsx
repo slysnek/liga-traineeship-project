91dd4795f9489b23db4861a0cb5507050344adcf
Kirill <bloodwingest@gmail.com>
2023-11-13 23:56:38 +0300

    feat: add params to search

--- src/app/Search/Search.tsx
+++ src/app/Search/Search.tsx
@@ -1,31 +1,38 @@
 import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import searchIcon from '../../assets/icons/icon-search.svg';
 import styles from './Search.module.css';
 import { useAppDispatch, useAppSelector } from 'src/hooks/hooks';
 import { changeFilters, getTasksQuery } from 'src/store/tasksSlice';
 
 const Search: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const reduxFilters = useAppSelector((state) => state.tasksInStore.filters);
   const dispatch = useAppDispatch();
 
+  const searchQuery = searchParams.get('searchQuery');
+
+  useEffect(() => {
+    if (searchQuery) {
+      dispatch(changeFilters({ ...reduxFilters, name_like: searchQuery }));
+    }
+  }, [searchQuery]);
+
   const handleTyping = (searchValue: string) => {
+    setSearchParams({ searchQuery: searchValue });
     dispatch(changeFilters({ ...reduxFilters, name_like: searchValue }));
-  };
-  const handleSearch = () => {
-    dispatch(getTasksQuery(reduxFilters));
   };
 
   return (
     <div className={styles['search-wrapper']}>
       <input
         onChange={(e) => handleTyping(e.target.value)}
-        value={reduxFilters.name_like}
         placeholder="Task name"
+        value={reduxFilters.name_like || ''}
         required
         className={styles['search-window']}
         type="text"
       />
-      <img onClick={handleSearch} className={styles['search-icon']} src={searchIcon} alt="search icon" />
     </div>
   );
 };
