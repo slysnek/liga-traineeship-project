90ea6b2c17ee3d2be03a96f6b26877a354b378ac
Kirill <bloodwingest@gmail.com>
2023-11-13 15:50:06 +0300

    feat: renew 404 page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,13 @@
+import dissapear from '../../assets/gif/dissapear.gif';
 import styles from './NotFound.module.css';
 
 const NotFound = () => {
-  return <h2>NotFound page</h2>;
+  return (
+    <div className={styles.notFound}>
+      <h2>Page was not found!</h2>
+      <img className={styles.scream} src={dissapear} alt="screaming and vanishing emoji" />
+    </div>
+  );
 };
 
 export default NotFound;
