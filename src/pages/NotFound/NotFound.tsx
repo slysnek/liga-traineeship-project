7b5c933df1324f3caf6fbf9e7a6368be80c656a4
Kirill <bloodwingest@gmail.com>
2023-11-13 16:06:08 +0300

    feat: add button to 404 for ux

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,9 +1,11 @@
 import dissapear from '../../assets/gif/dissapear.gif';
 import styles from './NotFound.module.css';
+import TitleLink from 'components/TitleLink/TitleLink';
 
 const NotFound = () => {
   return (
     <div className={styles.notFound}>
+      <TitleLink link="/" buttonName="Return me back!"></TitleLink>
       <h2>Page was not found!</h2>
       <img className={styles.scream} src={dissapear} alt="screaming and vanishing emoji" />
     </div>
