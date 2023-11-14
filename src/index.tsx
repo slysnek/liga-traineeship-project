093408212ed102b4547b2d3015b61c2bfb668497
Kirill <bloodwingest@gmail.com>
2023-11-15 00:06:27 +0300

    fix: fix css import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'index.css';
+import 'src/index.css';
 import { Provider } from 'react-redux';
 import { WrappedApp } from 'app/App';
 import { store } from 'src/store/store';
