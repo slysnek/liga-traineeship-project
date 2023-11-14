e73b9ff3c12ebfc5709b316b7c8c59929e239933
Kirill <bloodwingest@gmail.com>
2023-11-14 16:38:45 +0300

    feat: add search params to filter, delete all button

--- src/app/FIlters/Filters.tsx
+++ src/app/FIlters/Filters.tsx
@@ -1,24 +1,49 @@
+import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
 import styles from './Filters.module.css';
 import { useAppDispatch, useAppSelector } from 'src/hooks/hooks';
-import { changeFilters, getTasksQuery } from 'src/store/tasksSlice';
+import { changeFilters } from 'src/store/tasksSlice';
 import { GetFilteredTasksQuery } from 'api/apiTypes';
+import { convertToFilterValue } from 'utils/convertToFilterValue';
 
 const Filters: React.FC = () => {
+  const [searchFilterParams, setFilterParams] = useSearchParams();
   const reduxFilters = useAppSelector((state) => state.tasksInStore.filters);
   const dispatch = useAppDispatch();
+
+  const isImportantQuery = searchFilterParams.get('isImportant');
+  const isCompletedQuery = searchFilterParams.get('isCompleted');
+
+  useEffect(() => {
+    if (isImportantQuery) {
+      dispatch(changeFilters({ isImportant: convertToFilterValue(isImportantQuery) }));
+    }
+    if (isImportantQuery === '' || isImportantQuery === null) {
+      dispatch(changeFilters({ isImportant: undefined }));
+    }
+  }, [isImportantQuery]);
+
+  useEffect(() => {
+    if (isCompletedQuery) {
+      dispatch(changeFilters({ isCompleted: convertToFilterValue(isCompletedQuery) }));
+    }
+    if (isCompletedQuery === '' || isCompletedQuery === null) {
+      dispatch(changeFilters({ isCompleted: undefined }));
+    }
+  }, [isCompletedQuery]);
 
   const handleFilter = (filterKey: keyof GetFilteredTasksQuery) => {
     const currentValue = reduxFilters[filterKey];
 
     if (currentValue === undefined) {
-      dispatch(changeFilters({ ...reduxFilters, [filterKey]: true }));
-      dispatch(getTasksQuery({ ...reduxFilters, [filterKey]: true }));
+      searchFilterParams.set(String(filterKey), 'true');
+      setFilterParams(searchFilterParams);
     } else if (currentValue === true) {
-      dispatch(changeFilters({ ...reduxFilters, [filterKey]: false }));
-      dispatch(getTasksQuery({ ...reduxFilters, [filterKey]: false }));
+      searchFilterParams.set(String(filterKey), 'false');
+      setFilterParams(searchFilterParams);
     } else {
-      dispatch(changeFilters({ ...reduxFilters, [filterKey]: undefined }));
-      dispatch(getTasksQuery({ ...reduxFilters, [filterKey]: undefined }));
+      searchFilterParams.delete(String(filterKey));
+      setFilterParams(searchFilterParams);
     }
   };
 
@@ -32,11 +57,6 @@ const Filters: React.FC = () => {
 
   return (
     <div className={styles['filters-wrapper']}>
-      {/*       <button
-        className={styles['filter-button']}
-        onClick={() => setFilters({ ...reduxFilters, isImportant: undefined, isCompleted: undefined })}>
-        All
-      </button> */}
       <button
         className={`${styles['filter-button']} ${getButtonStyle(reduxFilters.isImportant)}`}
         onClick={() => handleFilter('isImportant')}>
